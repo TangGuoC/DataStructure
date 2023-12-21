6c2238362e9003ac58f7171a07f1a4e401d71072
TangGuoC <2958242107@qq.com>
2023-12-21 16:05:29 +0800

    [修改]完善指针的释放API接口

--- LinkList/main.c
+++ LinkList/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include "LinkList.h"
+#include <string.h>
 
+#define BUFFER_SIZE 20
 int main()
 {
-    
-
+#if 0
+//strcpy第一个参数是传出参数 并返回值也是直线dst的地址
+    char buffer[BUFFER_SIZE] = {0};
+    char *ptr = strcpy(buffer, "zhangssan");
+    printf("buffer:%s\n", buffer);
+    printf("ptr:%s\n", ptr);
+#endif
     return 0;
 }
